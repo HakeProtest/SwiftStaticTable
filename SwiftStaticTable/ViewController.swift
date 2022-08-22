2c810470cec2266942d4a39a1911c27eed9c3722
Hafizh Caesandro Kevin <hake.protest@gmail.com>
2022-08-22 16:15:32 +0700

    Initial Commit

---
+++ SwiftStaticTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftStaticTable
+//
+//  Created by Hafizh Caesandro Kevinoza on 22/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
